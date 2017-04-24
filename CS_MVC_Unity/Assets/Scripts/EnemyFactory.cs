f5485719c89cb9e3b6676d91268f48184730c2c7
William Brinkert <william.brinkert@sfm3lrg8wml.na.akqa.local>
2017-04-24 12:52:20 -0700

    feat: add enemy factories for mvc

---
+++ CS_MVC_Unity/Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,63 @@
+﻿// Interface for the model factory
+public interface IEnemyModelFactory
+{
+	// Get the created model
+	IEnemyModel model { get; }
+}
+
+// Implementation of the model factory
+public class EnemeyModelFactory : IEnemyModelFactory
+{
+	public IEnemyModel Model { get; private set; }
+
+	// create the model
+	public IEnemyModelFactory()
+	{
+		Model = new EnemyModel();
+	}
+}
+
+// interface for the view factory
+public interface IEnemyViewFactory
+{
+	// Get the created view
+	IEnemyView View { get; }
+}
+
+public class EnemyViewFactory : IEnemyViewFactory
+{
+	public IEnemyView View { get; private set; }
+
+	// Create the view
+	public IEnemyViewFactory()
+	{
+		var prefab = Resources.Load<GameObject>("Enemy");
+		var instance = UnityEngine.Object.Instantiate(prefab);
+		View = instance.GetComponent<IEnemyView>();
+	}
+}
+
+// interface of the view factory
+public interface IEnemyControllerFactory
+{
+	// Get the created controller
+	IEnemyController Controller { get; }
+}
+
+// Implementation of the controller factory
+public class EnemyControllerFactory : IEnemyControllerFactory
+{
+	public IEnemyController Controller { get; private set; }
+
+	// create just the controller
+	public EnemyControllerFactory(IEnemyModel model, IEnemyView view)
+	{
+		Controller = new EnemyController (model, view);
+	}
+
+	public EnemyControllerFactory()
+		: this(new EnemyModel(), new EnemyView())
+	{
+	}
+
+}
