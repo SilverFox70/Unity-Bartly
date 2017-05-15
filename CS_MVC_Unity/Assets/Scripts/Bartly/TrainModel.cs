9712669d2c8dbaef1a48d1f4c3340616dcb2deab
William Brinkert <william.brinkert@sfm3lrg8wml.na.akqa.local>
2017-05-15 16:32:20 -0700

    wip: add schedule and stop classes

--- CS_MVC_Unity/Assets/Scripts/Bartly/TrainModel.cs
+++ CS_MVC_Unity/Assets/Scripts/Bartly/TrainModel.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrainModel : MonoBehaviour {
-
+public class TrainModel : MonoBehaviour 
+{
+	public int trainId;
+	public int trainIdx;
 	public Vector3 startPos;
 
 	// Use this for initialization
---
+++ CS_MVC_Unity/Assets/Scripts/Bartly/TrainSchedule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainSchedule : MonoBehaviour {
+
+	public List<TrainStop> trainStops;
+	public int routeNumber;
+	public string routeName;
+
+	public TrainSchedule(int r_number, string r_name)
+	{
+		routeNumber = r_number;
+		routeName = routeName;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddStop(TrainStop trainStop)
+	{
+		trainStops.Add (trainStop);
+	}
+		
+}
