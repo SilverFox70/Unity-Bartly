bec20869a29752d51bfa56b8a3ff2b2b80004719
William Brinkert <william.brinkert@sfm3lrg8wml.na.akqa.local>
2017-05-17 09:32:35 -0700

    wip: various attempts at trainManager logic anf flow

--- CS_MVC_Unity/Assets/Scripts/Bartly/TrainManager.cs
+++ CS_MVC_Unity/Assets/Scripts/Bartly/TrainManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class TrainManager : MonoBehaviour {
 
 	private TrainStop lastStop = null;
+	private int index = 0;
+	private TrainSchedule trainSchedule;
+	private GameObject trainSchedObject;
+	private TrainMovement trainMovement;
 
 	// Use this for initialization
 	void Start () {
@@ -21,32 +25,45 @@ public class TrainManager : MonoBehaviour {
 	{
 		var prefab = Resources.Load<GameObject> ("Train");
 		var train = UnityEngine.Object.Instantiate (prefab);
-		TrainMovement trainMovement = train.GetComponent<TrainMovement> ();
-		GameObject trainSched = GameObject.Find ("Train3630417");
-		TrainSchedule trainSchedule = trainSched.GetComponent<TrainSchedule> ();
+		trainMovement = train.GetComponent<TrainMovement> ();
+		trainSchedObject = GameObject.Find ("Train3630417");
+		trainSchedule = trainSchedObject.GetComponent<TrainSchedule> ();
+		IterateStops (train);
 
-
-		foreach (TrainStop tstop in trainSchedule.trainStops) 
-		{
-			StartCoroutine (SendMoveData (tstop, trainMovement, train));
-		}
+//		foreach (TrainStop tstop in trainSchedule.trainStops) 
+//		{
+//			StartCoroutine (SendMoveData (tstop, trainMovement, train));
+//		}
 	}
 
-	IEnumerator SendMoveData(TrainStop trainStop, TrainMovement trainMovement, GameObject train)
+	public void IterateStops(GameObject train)
 	{
-		
-		if (lastStop == null) {
-			lastStop = trainStop;
-		} else {
-			double timeDiff = (trainStop.origTime - lastStop.origTime).TotalMinutes;
-			Vector3 lastStation = GameObject.Find (lastStop.stationName).transform.position;
-			Vector3 nextStation = GameObject.Find (trainStop.stationName).transform.position;
-			trainMovement.MoveTrain (train, lastStation, nextStation, timeDiff);
-			while (trainMovement.moving) 
-			{
-				yield return null;
-			}
-
+		if (trainSchedule [index] != null && trainSchedule [index + 1] != null && !trainMovement.moving) 
+		{
+			TryTrainMove(trainSchedule[index], trainSchedule[ index + 1], train); 
 		}
 	}
+
+	public void TryTrainMove(TrainStop thisStop, TrainStop nextStop, GameObject train)
+	{
+		trainMovement.MoveTrain ();
+	}
+
+//	IEnumerator SendMoveData(TrainStop trainStop, TrainMovement trainMovement, GameObject train)
+//	{
+//		
+//		if (lastStop == null) {
+//			lastStop = trainStop;
+//		} else {
+//			double timeDiff = (trainStop.origTime - lastStop.origTime).TotalMinutes;
+//			Vector3 lastStation = GameObject.Find (lastStop.stationName).transform.position;
+//			Vector3 nextStation = GameObject.Find (trainStop.stationName).transform.position;
+//			trainMovement.MoveTrain (train, lastStation, nextStation, timeDiff);
+//			while (trainMovement.moving) 
+//			{
+//				yield ;
+//			}
+//
+//		}
+//	}
 }
